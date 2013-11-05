2ad1ba13c990bd333509f47bd3c31a8a80da0d18
a0-prw <pete_wood@runbox.com>
2013-11-05 14:10:16 +0100

    Export ccydb-date

--- package.lisp
+++ package.lisp
@@ -34,4 +34,5 @@
            :lookup-currency :ccy-name :ccy-ddigits 
            :ccy-ncode :ccy-acode :ccy-country
            :display-currency :with-currency          
+           :ccydb-date
            :*stored-currency-hash*))
